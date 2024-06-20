9251084228b2d5367b707b77085506bdd4c2752a
Saurabh Sharma <128302506+saurabhjb007@users.noreply.github.com>
2024-06-20 12:16:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.use("/images", express.static("./upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `https://shopease-clothing.onrender.com/images/${req.file.filename}`,
+    image_url: `https://shopease-clothing.onrender.com//images/${req.file.filename}`,
   });
 });
 
